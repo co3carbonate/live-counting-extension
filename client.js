17a96b10958bfaf5d2cbe18088f6a7fc2bfa5ef8
co3carbonate <hp.decifit@gmail.com>
2016-12-22 15:23:29 +0800

    Added client.js
    The contents of the client's TamperMonkey/GreaseMonkey script to link to
    main.js

---
+++ client.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Live Counting Extension
+// @namespace    http://tampermonkey.net/
+// @version      1.1
+// @description  Add-on features for Live Counting
+// @author       /u/co3_carbonate
+// @website      https://github.com/co3carbonate/live-counting-extension
+// @match        https://www.reddit.com/live/ta535s1hq2je
+// @match        https://www.reddit.com/live/ta535s1hq2je/
+// @require      https://cdn.rawgit.com/co3carbonate/live-counting-extension/master/main.js
+// @grant        none
+// ==/UserScript==
