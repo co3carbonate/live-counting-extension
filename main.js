128a7b207247c189ca611bd2179aa63837f48a65
rideride <iamrideride@gmail.com>
2018-10-03 20:12:39 -0500

    MaybeNotWrong 100k username
    This one is for winning 9500-9599k. He still has one for 9600-9699k and possibly for 9700-9799k. This one was added at about 9800k.

--- main.js
+++ main.js
@@ -11,6 +11,39 @@ var USER = $('#header .user a[href]').html();
 var chu = 0;
 var count = 0;
 var s = "leinad";
+var maybenumbers = {
+    "001":"/u/rschaosid",
+    "008":"/u/gordonpt8",
+    "018":"/u/Kris18",
+    "036":"/u/Iamspeedy36",
+    "037":"/u/amazingpikachu_38",
+    "047":"/u/kdiuro13",
+    "100":"/u/abplows",
+    "123":"random123",
+    "151":"/u/MewDP",
+    "200":"/u/QuestoGuy",
+    "212":"/u/MrBahr12",
+    "217":"/u/Lonadont",
+    "220":"/u/Chalupa_Dad",
+    "222":"/u/treje",
+    "234":"/u/ElliottB1",
+    "301":"/u/piyushsharma301",
+    "333":"/u/Majestic_Bear",
+    "369":"/u/TOP_20",
+    "404":"random404",
+    "471":"/u/albert471",
+    "501":"/u/LeinadSpoon",
+    "515":"/u/NobodyL0vesMe",
+    "555":"/u/PrinceCrinkle",
+    "615":"/u/parker_cube",
+    "616":"/u/DemonBurritoCat",
+    "666":"/u/rideride",
+    "689":"/u/smarvin6689",
+    "777":"/u/artbn",
+    "845":"/u/noduorg",
+    "973":"/u/Smartstocks",
+    "998":"/u/qwertylool"
+};
 
 // Thread ID
 var THREAD = (function () {
@@ -4288,50 +4321,29 @@ userColors.LC_Chats = 'white';
 		
 // /u/MaybeNotWrong username special
 if (data.author == 'MaybeNotWrong') {
-var mayberand = Math.floor((Math.random() * 2) + 1);
-var notrand = Math.floor((Math.random() * 2) + 1);
-var wrongrand = Math.floor((Math.random() * 2) + 1);
-var colorrand = Math.floor((Math.random() * 2) + 1);
-var okcolor = '#e02';
-var okmaybe = 'Maybe';
-var oknot = 'Not';
-var okwrong = 'Wrong';
- if (mayberand == 1) {
-      okmaybe = '';
+    var count1testlol = data.body_elem.text();
+count1testlol = count1testlol.substring(0, 9);
+    count1testlol = count1testlol.replace(/[A-Za-z]/g, '');
+    count1testlol = count1testlol.replace(/,/g, '');
+    count1testlol = count1testlol.replace(/ /g, '');
+count1testlol = count1testlol.substr(count1testlol.length - 3);
+var maybename = maybenumbers[count1testlol];
+if (maybename == null) {
+maybename = '/u/MaybeNotWrong';
+var maybecolor = '#066666';
+} else {
+if (maybename == "random123") {var randomx = Math.floor((Math.random() * 2) + 1);if (randomx == 1) {maybename = '/u/davidjl123';} else {maybename = '/u/dominodan123';}}
+if (maybename == "random404") {var randomx = Math.floor((Math.random() * 2) + 1);if (randomx == 1) {maybename = '/u/Tranquilsunrise';} else {maybename = '/u/KingCaspianX';}}
+var maybenamet = maybename.trim().replace('/u/', '');;
+var maybecolor = userColors[maybenamet];
 }
-if (notrand == 1) {
-      oknot = '';
-}
-if (wrongrand == 1) {
-      okwrong = 'Right';
-}
-
-if (mayberand === 1) {
-if (notrand === 2) {
-if (wrongrand === 2) {
-okcolor = '#0f4';
-}}}
-
-if (mayberand === 1 && notrand === 1 && wrongrand === 1) {
-okcolor = '#0f4';
-}
-
-
-
-if (mayberand == 2) {
-if (colorrand == 1) {
-okcolor = '#0f4';
-}
-}
-
  data.author_elem.css({
                   'font-weight': 'bold',
                   'display': 'inline-block'
               });
-             data.author_elem.html(`<div style="color:`+okcolor+`;">`+okmaybe+oknot+okwrong+`</div>`)
-
-return;
-        }
+data.author_elem.html(`<div style="color:`+maybecolor+`;">`+maybename+`</div>`)
+            
+}
 	
 
 
