e0e06228e5085c52de39006a2c6d8d92c388a499
haykam821 <24855774+haykam821@users.noreply.github.com>
2021-04-16 18:35:18 -0400

    Allow Webpack to transpile TypeScript files

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,33 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+
 const path = require("path");
+
+const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 
 module.exports = {
 	entry: "./main.js",
 	mode: process.env.WEBPACK_MODE || "production",
+	module: {
+		rules: [{
+			include: path.resolve(__dirname, "./src"),
+			loader: "ts-loader",
+			options: {
+				transpileOnly: true,
+			},
+			test: /\.ts$/,
+		}],
+	},
 	output: {
 		filename: "main.js",
 		path: path.resolve(__dirname, "./dist"),
+	},
+	plugins: [
+		new ForkTsCheckerWebpackPlugin(),
+	],
+	resolve: {
+		extensions: [
+			".js",
+			".ts",
+		],
 	},
 };
