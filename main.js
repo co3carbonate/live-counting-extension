44d354f205598150100e3e393289bd2dc2fa569a
ItzTaken <itztaken.mail@gmail.com>
2021-04-23 10:29:10 -0400

    Remove thread selector when LC Chats view is turned off

--- main.js
+++ main.js
@@ -3023,9 +3023,11 @@ var LC_Chats_View;
         else if(h == 'yeah bro' && lccTest == 2) {
             lccTest = 1;
             $('#lc_chats_iframe').css('display','initial');
+            $('#iframe_loader').css('display','initial');
         } else if(h == 'nuh uh bro, also does this piss you off that im using strings like this? GOOD.' && lccTest == 1) {
             lccTest = 2;
             $('#lc_chats_iframe').css('display','none');
+            $('#iframe_loader').css('display','none');
         }
     }
     Options.addCheckbox({
