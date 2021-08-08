6f3b4e9c12de6600d8b5224c5b602f82e7458bc1
MaybeNotWrong <37958125+MaybeNotWrong@users.noreply.github.com>
2021-08-08 11:36:41 +0200

    Implement suggestions
    Moved onChange logic back since it works there

--- src/rate-limit-view/rate-limit-view.ts
+++ src/rate-limit-view/rate-limit-view.ts
@@ -1,89 +1,85 @@
-import COLORS from "../data/rate-limit-view-colors.json";
-
-interface RateLimitColors {
-	negative: string,
-	positive: string[]
-}
-
-const RATE_LIMIT_COLORS: RateLimitColors = COLORS;
+import { RATE_LIMIT_COLORS } from "./rate-limit-colors";
 const RATE_LIMIT = 333;
 const COLOR_INTERVAL = 20;
 const COLOR_MAX_MS = RATE_LIMIT_COLORS.positive.length * COLOR_INTERVAL;
 
 export class RateLimitView {
-	static oldtime = Date.now();
-	static newtime = Date.now();
-	static bgColor = "";
-	static fontColor = "";
-	static isDelta = false;
-	static isEnabled = false;
+	oldtime = Date.now();
+	newtime = Date.now();
+	isDelta = false;
+	isEnabled = false;
 
 	constructor(Options: any) {
-		$("<span id=ratelim class=ratelimit>RL: <span id=rate></span></span>").insertBefore(".CANT_REPLY");
-		$(".ratelimit").css({
-			background: "transparent",
-			display: "none",
-			float: "right",
-			"font-size": "smaller",
-			"margin-left": "10px",
-			"margin-top": "5px",
-		});
+		$("<span id=ratelim class=ratelimit>RL: <span id=rate></span></span>")
+			.insertBefore(".CANT_REPLY")
+			.css({
+				background: "transparent",
+				display: "none",
+				float: "right",
+				"font-size": "smaller",
+				"margin-left": "10px",
+				"margin-top": "5px",
+			});
 		this.initOptions(Options);
 	}
 
 	initOptions(Options: any): void {
+		const self = this;
 		Options.addSelect({
 			default: 0,
 			help: "Rate limit viewer by post. Not real, just estimate lol",
 			label: "RATE LIMIT VIEW",
-			onchange: RateLimitView.onChange.call(this),
+			onchange: function() {
+				const val = this.val();
+				if(val == "Normal" || val == "Delta") {
+					$(".ratelimit").css("display", "initial");
+					$(".help-toggle").css("display", "none");
+					self.isEnabled = true;
+				}
+				if(val == "Disabled") {
+					$(".ratelimit").css("display", "none");
+					$(".help-toggle").css("display", "initial");
+					$(".bottom-area").css({
+						background: "transparent",
+						color: "",
+					});
+					self.isEnabled = false;
+				}
+				self.isDelta = val == "Delta";
+			},
 			options: ["Disabled", "Normal", "Delta"],
 			section: "Advanced 2",
 		});
 
-		$("#new-update-form .save-button button").on("click", RateLimitView.onMakeUpdate);
+		$("#new-update-form .save-button button").on("click", this.onMakeUpdate.bind(this));
 	}
 
-	private static onChange(): void {
-		const val = this.val();
-		if(val == "Normal" || val == "Delta") {
-			$(".ratelimit").css("display", "initial");
-			$(".help-toggle").css("display", "none");
-			RateLimitView.isEnabled = true;
-		}
-		if(val == "Disabled") {
-			$(".ratelimit").css("display", "none");
-			$(".help-toggle").css("display", "initial");
-			$(".bottom-area").css("background", "transparent");
-			$(".bottom-area").css("color", "");
-			RateLimitView.isEnabled = false;
-		}
-		RateLimitView.isDelta = val == "Delta";
-	}
-
-	private static onMakeUpdate(): void {
+	private onMakeUpdate(): void {
 		// Set times in any case to ensure correct values after turning it on
-		RateLimitView.oldtime = RateLimitView.newtime;
-		RateLimitView.newtime = Date.now();
-		if(!RateLimitView.isEnabled)return;
+		this.oldtime = this.newtime;
+		this.newtime = Date.now();
+		if(!this.isEnabled)return;
 		// Compute relevant time differences
-		const selfReplyTime = RateLimitView.newtime - RateLimitView.oldtime;
+		const selfReplyTime = this.newtime - this.oldtime;
 		const delta = selfReplyTime - RATE_LIMIT;
 		// Set Background Color
+		let bgColor: string;
 		if(delta < 0) {
-			RateLimitView.bgColor = RATE_LIMIT_COLORS.negative;
+			bgColor = RATE_LIMIT_COLORS.negative;
 		} else if (delta < COLOR_MAX_MS) {
 			const index = Math.floor(delta / COLOR_INTERVAL);
-			RateLimitView.bgColor = RATE_LIMIT_COLORS.positive[index];
+			bgColor = RATE_LIMIT_COLORS.positive[index];
 		} else {
-			RateLimitView.bgColor = "transparent";
+			bgColor = "transparent";
 		}
 		// Set Font Color
-		RateLimitView.fontColor = delta < COLOR_MAX_MS ? "#000" : "";
+		const fontColor = delta < COLOR_MAX_MS ? "#000" : "";
 		// Update UI
-		const value = RateLimitView.isDelta ? delta : selfReplyTime;
+		const value = this.isDelta ? delta : selfReplyTime;
 		$("#rate").text(value + "ms");
-		$(".bottom-area").css("background", RateLimitView.bgColor);
-		$(".bottom-area").css("color", RateLimitView.fontColor);
+		$(".bottom-area").css({
+			background: bgColor,
+			color: fontColor,
+		});
 	}
 }
