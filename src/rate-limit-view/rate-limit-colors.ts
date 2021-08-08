96ee2c1f8d55db13fb0464cd5fff53def151172c
MaybeNotWrong <37958125+MaybeNotWrong@users.noreply.github.com>
2021-08-08 11:36:41 +0200

    Implement suggestions
    Moved onChange logic back since it works there

---
+++ src/rate-limit-view/rate-limit-colors.ts
@@ -0,0 +1,8 @@
+import COLORS from "../data/rate-limit-view-colors.json";
+
+interface RateLimitColors {
+	negative: string,
+	positive: string[]
+}
+
+export const RATE_LIMIT_COLORS: RateLimitColors = COLORS;
