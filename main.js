8106f974d8595163e5cc73fffaae84a9ad5e0b97
rideride <7028133+rideride@users.noreply.github.com>
2025-12-30 23:05:14 -0500

    fix bold usernames

--- main.js
+++ main.js
@@ -1611,7 +1611,7 @@ var ColoredUsernames;
     if (!enabled) return;
     // Special usernames (temp rewards for top in 100k HoC, or other contributions)
     // Bot-maker privileges
-    if (BOLD_USERS.has(thisauthor)) $(this).css('font-weight', 'bold');
+    if (BOLD_USERS.has(data.author)) $(this).css('font-weight', 'bold');
 
     // 100K usernames
 
